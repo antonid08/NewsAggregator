c3dd978070511165ebccf7cee0705ef7856410e9
iantonenko <iantonenko@arlo.com>
2021-12-14 00:28:43 +0300

    App skeleton added

---
+++ application/app/src/main/java/antonid/newsaggregator/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package antonid.newsaggregator.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import antonid.newsaggregator.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
